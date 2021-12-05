ed02232ce918418f37ebe3c81867abd622225e06
marvin <marvinsevilla05@gmail.com>
2021-12-05 20:48:42 +0000

    anexe algunos documentos sobre el curso de C
    se agregaron documentos txt a la carpeta cursos
    
    tambien se agregaron las funciones de los botones de los cursos de C

--- NetCode_Proyect/static/scripts/scriptCursos.js
+++ NetCode_Proyect/static/scripts/scriptCursos.js
@@ -27,3 +27,33 @@ $("#C_4").on('click', function(){
    $("#tema").load('/static/cursos/C_tema_4.txt')
 
 });
+
+$("#C_5").on('click', function(){
+
+   $("#tema").load('/static/cursos/C_tema_5.txt')
+
+});
+
+$("#C_6").on('click', function(){
+
+   $("#tema").load('/static/cursos/C_tema_6.txt')
+
+});
+
+$("#C_7").on('click', function(){
+
+   $("#tema").load('/static/cursos/C_tema_7.txt')
+
+});
+
+$("#C_8").on('click', function(){
+
+   $("#tema").load('/static/cursos/C_tema_8.txt')
+
+});
+
+$("#C_9").on('click', function(){
+
+   $("#tema").load('/static/cursos/C_tema_9.txt')
+
+});
