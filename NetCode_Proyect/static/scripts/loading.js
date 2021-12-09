2ab902572a63e0e992362448edc45635a33c5546
marvin <marvinsevilla05@gmail.com>
2021-12-09 23:40:19 +0000

    se ha mejorado la pagina para espera mientras se carga Cursos.html
    en concreto se agrego un div que a=carga una imagen de cargando, un css para mostrar ese div y un js que carga el contenido del div y luego lo oculta

---
+++ NetCode_Proyect/static/scripts/loading.js
@@ -0,0 +1,7 @@
+    $(window).on('load',function() {
+
+        var contenedor = document.getElementById('jm-loadingpage');
+
+        contenedor.style.visibility = 'hidden';
+        contenedor.style.opacity = '0';
+    });
